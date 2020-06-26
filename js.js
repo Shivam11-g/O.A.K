dff4838d0e86ac2225f91ddd728ce53ba977dcdb
Shivam11-g <54078532+Shivam11-g@users.noreply.github.com>
2020-06-26 15:07:08 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,90 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1;}    
+  
+  slides[slideIndex-1].style.display = "block";  
+  
+  setTimeout(showSlides, 6000); // Change image every 7 seconds
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "400px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+var slide2Index = 0;
+Slides();
+function Slides() {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slide2Index++;
+  if (slide2Index > slides.length) {slide2Index = 1}    
+  
+  slides[slide2Index-1].style.display = "block";  
+  
+  setTimeout(Slides, 3000); // Change image every 3 seconds
+}
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//FOR BUTTONS (animated buttons)
+function myFunction(x) {
+  x.classList.toggle("change");
+ 
+
+}
+
+var flag=0;
+
+function linkTableAnimation(){
+  var a= document.getElementsByClassName("linkAnimation");
+  var i;
+if (flag>0) {
+    for ( i = 0; i < a.length; i++) {
+    a[i].style.animation="reverseAnimation 0.5s";
+    a[i].style.left='-625px';
+    }
+    flag=0;
+  }
+else{
+    for ( i = 0; i < a.length; i++) {
+    a[i].style.animation="linkanimi 0.5s";
+    a[i].style.left='150px';
+    }
+    flag=1;
+  }
+}
+
